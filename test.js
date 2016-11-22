83338cd8844fa11b56b979a3e72554bd6426e1e9
E. Weilow <eweilow@digitalkoppar.se>
2016-11-22 22:20:18 +0100

    Fix test :+1:

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const index = require("./index.js");
+const assert = require("chai").assert;
 index.UpperSecondary.parseTGZ("./src/syllabus.tgz")
   .then(upperSecondaryData => {
+    assert.isDefined(upperSecondaryData.courses);
+    assert.isDefined(upperSecondaryData.subjects);
     //console.log(upperSecondaryData);
-  });
+  }).catch(err => {
+    assert.fail(err);
+  })
