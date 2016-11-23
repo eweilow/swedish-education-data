4d9e5f9b4af1f9ab279cb80de90e01121f9cfa9f
E. Weilow <eweilow@digitalkoppar.se>
2016-11-23 20:56:51 +0100

    Begin parsing actual subject data
    It's bad. Two edge cases so far requiring custom code. :-1:

--- test.js
+++ test.js
@@ -2,9 +2,8 @@ const index = require("./index.js");
 const assert = require("chai").assert;
 index.UpperSecondary.parseTGZ("./src/syllabus.tgz")
   .then(upperSecondaryData => {
-    assert.isDefined(upperSecondaryData.courses);
-    assert.isDefined(upperSecondaryData.subjects);
+    //assert.isDefined(upperSecondaryData);
     //console.log(upperSecondaryData);
   }).catch(err => {
-    assert.fail(err);
+    assert.fail(err.message, "", err.stack);
   })
