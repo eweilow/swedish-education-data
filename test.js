47b0752e4d0336eb723ff59a062fd446eed0d647
E. Weilow <eweilow@digitalkoppar.se>
2016-12-03 17:38:04 +0100

    Add parsing of programs!

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ index.UpperSecondary.parseTGZ("./src/syllabus.tgz")
     assert.isDefined(upperSecondaryData);
     assert.isDefined(upperSecondaryData.courses);
     assert.isDefined(upperSecondaryData.subjects);
+    assert.isDefined(upperSecondaryData.programs);
     assert.isAtLeast(upperSecondaryData.courses.size, 1);
     assert.isAtLeast(upperSecondaryData.subjects.size, 1);
+    assert.isAtLeast(upperSecondaryData.programs.size, 1);
   }).catch(err => {
     assert.fail(err.message, "", err.stack);
   })
