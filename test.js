7b8950d5f577db386e87c31bb2e107902ee4fd05
E. Weilow <eweilow@digitalkoppar.se>
2016-12-03 16:06:24 +0100

    Properly finish course and subject parsing for Upper Secondary level school

--- test.js
+++ test.js
@@ -2,8 +2,11 @@ const index = require("./index.js");
 const assert = require("chai").assert;
 index.UpperSecondary.parseTGZ("./src/syllabus.tgz")
   .then(upperSecondaryData => {
-    //assert.isDefined(upperSecondaryData);
-    //console.log(upperSecondaryData);
+    assert.isDefined(upperSecondaryData);
+    assert.isDefined(upperSecondaryData.courses);
+    assert.isDefined(upperSecondaryData.subjects);
+    assert.isAtLeast(upperSecondaryData.courses.size, 1);
+    assert.isAtLeast(upperSecondaryData.subjects.size, 1);
   }).catch(err => {
     assert.fail(err.message, "", err.stack);
   })
