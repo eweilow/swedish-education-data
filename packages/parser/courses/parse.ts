c6705be7f11d3ff2c75614b77e13b3fbf80b413b
E. Weilow <eweilow@digitalkoppar.se>
2019-10-02 12:33:23 +0200

    Parse grading criteria

--- packages/parser/courses/parse.ts
+++ packages/parser/courses/parse.ts
@@ -42,11 +42,74 @@ export async function parseCourse(
     );
   }
 
+  const reqs = data.knowledgeRequirements.map(el => ({
+    step: el.gradeStep[0],
+    text: el.text[0]
+  }));
+
+  const out = {};
+  for (const req of reqs) {
+    if (req.step === "D" || req.step === "B") {
+      continue;
+    }
+    if (
+      !/^\<h4\>[^<]+\<\/h4\>\s*(<div>\s*)*(?:\s*<p>(.+?)<\/p>)+(\s*<\/div>)*$/.test(
+        req.text
+      )
+    ) {
+      console.log(data.code[0] + " - " + req.step + ": \n" + req.text);
+    }
+    out[req.step] = [...req.text.matchAll(/<p>(.+?)<\/p>/g)]
+      .map(el => el[1])
+      .map(el =>
+        el
+          .replace(/<italic>\s*\.\s*<\/italic>/g, ".")
+          .replace(/\.\s*<\/strong>/g, "</strong>. ")
+          .split(".")
+          .map(
+            el =>
+              el
+                .trim()
+                .replace(/<strong>\s+/g, " <strong>")
+                .replace(/\s+<strong>/g, " <strong>")
+                .replace(/ <\/strong>/g, "</strong> ")
+                .replace(/<\/strong>\s+/g, "</strong> ") + "."
+          )
+
+          .filter(el => el !== ".")
+      );
+
+    for (const row of out[req.step]) {
+      for (const part of row) {
+        const matchedTags = new Set(
+          [...part.matchAll(/<([^/>]+)>/g)].map(el => el[1])
+        );
+
+        if (
+          matchedTags.size === 0 ||
+          (matchedTags.size === 1 && matchedTags.has("strong"))
+        ) {
+          continue;
+        }
+
+        throw new Error(
+          `Found invalid tags '${[...matchedTags].join(", ")}' in '${
+            req.text
+          }' in grade ${req.step} of course ${data.code[0]}`
+        );
+      }
+    }
+    // console.log();
+  }
+
+  //console.log(out);
+
   const course = {
     subject: subject.code,
     title: data.name[0].trim(),
     code: data.code[0].trim(),
     points: parseInt(data.point[0], 10),
+    criteria: out,
     // This is basically unsafe to use, because it's really inconsistent
     UNSAFE_description: $(
       "<div>" +
